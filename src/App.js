69e12289eb418053e8bc0c150ec19e4bd99cc9c4
Ankit Kumar Jha <132083151+Ankitkrjha32@users.noreply.github.com>
2024-02-11 14:57:53 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h2 className='text:blue'>Hello this is my project first</h2>
+    </div>
+  );
+}
+
+export default App;
